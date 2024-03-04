7f271a5890639c159e05cc46c51e1557628671bb
mucthecoder <140271183+mucthecoder@users.noreply.github.com>
2024-03-04 12:44:53 +0200

    Add files via upload

---
+++ java.js
@@ -0,0 +1,8 @@
+document.getElementById("but").style.backgroundColor='blue';
+
+document.getElementById("but").style.width='100px'
+document.getElementById("but").style.height='100px'
+
+function changecolor(){
+   setTimeout(document.body.style.backgroundColor='yellow',6000)
+}
